f9b4870a3f04f5ebb0d80057fe2623d013b0d553
Obicere <mattwzawislak@gmail.com>
2014-09-08 22:11:43 -0600

    Added new messages

--- src/org/obicere/cc/configuration/Message.java
+++ src/org/obicere/cc/configuration/Message.java
@@ -5,10 +5,20 @@ import java.util.Random;
 public class Message {
 
     private final static String[] MESSAGES = new String[]{
-            "Han shot first.",
-            "There is no cow level.",
-            "More polygons than before!",
-            "Now with 0 calories!"
+            //"Han shot first.",
+            //"There is no cow level.",
+            //"More polygons than before!",
+            //"Now with 0 calories!",
+            //"Rxd4!!",
+            //"PC Master Race",
+            //"Now with a O(nlogn) complexity!",
+            //"How Can Mirrors Be Real If Our Eyes Aren't Even Real?",
+            //"import javax.swing.*;",
+            //"i++;",
+            //"Isn't a parsec a unit of distance?",
+            //"0 < i, i + 1 < 0",
+            ""
+
     };
 
     private static final Random SEED = new Random();

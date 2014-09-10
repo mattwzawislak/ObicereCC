42eac824571a0b47faf753bcc6f00c5e2121214a
Obicere <mattwzawislak@gmail.com>
2014-09-09 22:25:19 -0600

    Added new settings panels

---
+++ src/org/obicere/cc/gui/settings/ButtonSetting.java
@@ -0,0 +1,35 @@
+package org.obicere.cc.gui.settings;
+
+import org.obicere.cc.shutdown.SettingsShutDownHook;
+
+import javax.swing.*;
+
+/**
+ * @author Obicere
+ */
+public class ButtonSetting extends SettingPanel {
+
+    private final String   action;
+    private final Runnable task;
+
+    public ButtonSetting(final SettingsShutDownHook hook, final String key, final String description, final String action, final Runnable task) {
+        super(hook, key, description);
+        this.action = action;
+        this.task = task;
+    }
+
+    @Override
+    protected void buildPanel() {
+
+        setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+
+        final JLabel descriptor = new JLabel(getDescriptor());
+        final JButton button = new JButton(action);
+
+        button.addActionListener(e -> task.run());
+
+        add(descriptor);
+        add(Box.createHorizontalGlue());
+        add(button);
+    }
+}
---
+++ src/org/obicere/cc/gui/settings/StringSetting.java
@@ -0,0 +1,55 @@
+package org.obicere.cc.gui.settings;
+
+import org.obicere.cc.shutdown.SettingsShutDownHook;
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+/**
+ * @author Obicere
+ */
+public class StringSetting extends SettingPanel {
+
+
+    public StringSetting(final SettingsShutDownHook hook, final String key, final String description) {
+        super(hook, key, description);
+    }
+
+    @Override
+    protected void buildPanel() {
+
+        final SettingsShutDownHook hook = getHook();
+        final String key = getKey();
+
+        setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+
+        final JLabel descriptor = new JLabel(getDescriptor());
+        final JTextField field = new JTextField(hook.getPropertyAsString(key)); // Set default
+
+        field.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(final DocumentEvent e) {
+                update();
+            }
+
+            @Override
+            public void removeUpdate(final DocumentEvent e) {
+                update();
+            }
+
+            @Override
+            public void changedUpdate(final DocumentEvent e) {
+                update();
+            }
+
+            private void update(){
+                hook.setProperty(key, field.getText());
+            }
+        });
+
+        add(descriptor);
+        add(Box.createHorizontalGlue());
+        add(field);
+    }
+}
